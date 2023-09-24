56f0f4fe35d1bcd02c4e35fb8f01761e29aabeea
Ashamsa Adhikari <ashamsaadhikari@gmail.com>
2023-09-24 09:17:14 -0500

    tried and failed on a navigation bar
    navigation bar did not work will try again.

---
+++ format.js
@@ -0,0 +1,6 @@
+const togglebutton = document.getElementById('togglebutton');
+const naviList = document.getElementById('naviList');
+
+togglebutton.addEventListener('click',() => {
+    naviList.classList.togglebutton('active');
+    })
