01b8fda1f8ebd4e1fc492896ddef756a4d00b01c
Ashamsa Adhikari <ashamsaadhikari@gmail.com>
2023-09-29 22:57:30 -0500

    Adding a new file called navbar
    navbar is going to be used as a base for my index.html file

--- format.js
+++
@@ -1,6 +0,0 @@
-const togglebutton = document.getElementById('togglebutton');
-const naviList = document.getElementById('naviList');
-
-togglebutton.addEventListener('click',() => {
-    naviList.classList.togglebutton('active');
-    })
